28537b48d95412f737e340c5e316213e9540d909
tjwjdgks <tjwjdgks43@gmail.com>
2020-05-21 11:49:46 +0900

    test

---
+++ server/src/main/java/board/controller/Controller.java
@@ -0,0 +1,18 @@
+package board.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+public class Controller {
+	@RequestMapping(value = "/test")
+	public String hello(HttpServletRequest httpServletRequest) {
+		String id = httpServletRequest.getParameter("id");
+		return "hello " + id;
+	}
+}

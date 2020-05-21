e798c0d893094ec02e181f03dc2f427598b63acf
tjwjdgks <tjwjdgks43@gmail.com>
2020-05-21 16:35:16 +0900

    test

--- src/main/java/com/insight/crescendo/controller/Controller.java
+++ src/main/java/com/insight/crescendo/controller/Controller.java
@@ -9,11 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RestController
 public class Controller {
-    @RequestMapping(value="/test")
-
-	public String hello(HttpServletRequest httpServletRequest) {
-
-		String id = httpServletRequest.getParameter("id");
-		return id;
-	}	
+	  @RequestMapping(value="/test")
+		public String hello(HttpServletRequest httpServletRequest) {
+			String id = httpServletRequest.getParameter("id");
+			return id;
+		}
 }

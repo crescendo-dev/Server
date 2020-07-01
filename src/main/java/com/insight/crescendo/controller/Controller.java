032af30e9ea890600b9f35c4ed157ba344e8d980
tjwjdgks <tjwjdgks43@gmail.com>
2020-07-02 00:23:06 +0900

    중간

--- src/main/java/com/insight/crescendo/controller/Controller.java
+++ src/main/java/com/insight/crescendo/controller/Controller.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.insight.crescendo.entity.account;
+import com.insight.crescendo.repository.accountRepostitory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RestController
 public class Controller {
+	  @Autowired
+	  private AccountService accountService;
+	  @Autowired
+	  private accountRepostitory accountRepostitory;
 	  @RequestMapping(value="/test")
 		public String hello(HttpServletRequest httpServletRequest) {
 			String id = httpServletRequest.getParameter("id");
@@ -22,6 +29,24 @@ public class Controller {
 		  Map<String,Object> map = new HashMap<>();
 		  String id = httpServletRequest.getParameter("id");
 		  String password = httpServletRequest.getParameter("password");
+		  
+		  return map;
+	  }
+	  @RequestMapping(value="/SignUp")
+	  public Map<String,Object> SignUp(HttpServletRequest httpServletRequest){
+		  Map<String, Object> map = new HashMap<String, Object>();
+		  String email = httpServletRequest.getParameter("email");
+		  String password = httpServletRequest.getParameter("password");
+		  String name = httpServletRequest.getParameter("name");
+
+		  account account = new account();
+		  account.setEmail(email);
+		  account.setPassword(password);
+		  account.setName(name);
+		  accountRepostitory.save(account);
+		  //int id = accountService.insertAccountInfo(email, password, name);
+		  Long id = account.getId();
+		  map.put("id", id);
 		  return map;
 	  }
 }

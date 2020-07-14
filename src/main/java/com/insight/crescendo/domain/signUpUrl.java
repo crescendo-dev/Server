80cd4aa2a03bd1cc4a6bd246696ba7231c1aab03
tjwjdgks <tjwjdgks43@gmail.com>
2020-07-14 19:36:42 +0900

    test

---
+++ src/main/java/com/insight/crescendo/domain/signUpUrl.java
@@ -0,0 +1,10 @@
+package com.insight.crescendo.domain;
+
+import lombok.Data;
+
+@Data
+public class signUpUrl {
+    private String name;
+    private String email;
+    private String password;
+}
